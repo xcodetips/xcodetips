a291eebb4af13fd4c4478bf05a2a5f025fbef331
Giovanni Lodi <giovanni.lodi42@gmail.com>
2018-04-04 16:55:28 +1000

    Remove timestamp from generated build folder
    It was there only to help me with diffing while iterating on the
    template. No need for that anymore.

--- generate.rb
+++ generate.rb
@@ -3,7 +3,7 @@ require 'yaml'
 
 def generate_tip_page(tip, tips, output_dir_path, output_name = nil)
   puts "Generating page for #{tip['title']}"
-  
+
   output_name ||= "#{tip['file_name']}.html"
 
   File.open("#{output_dir_path}/#{output_name}", 'w') do |file|
@@ -29,7 +29,7 @@ if tips.empty?
   exit 2
 end
 
-output_dir_path = "build_#{Time.now.getutc.to_i}"
+output_dir_path = "build"
 `mkdir #{output_dir_path}`
 `mkdir #{output_dir_path}/tips`
 
